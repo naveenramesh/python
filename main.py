1bda96b9ab35028af61a3b78e63f4a5cdecb03e0
naveenramesh <naveen.ramesh0@gmail.com>
2023-08-02 19:11:11 +0000

    selinium code snip added

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print ("hello")
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
+opt = Options()
+browser= Chrome(options=opt)
+browser.get("www.google.com")
